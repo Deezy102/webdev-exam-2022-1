4a30fc931a22b77baaa5633ea5efae4328336cc1
Vlad Nikolaev <vlad102nikolaev@gmail.com>
2022-01-28 03:06:57 +0300

    counters renderMenu and something else

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ let mainUrl = new URL('http://exam-2022-1-api.std-900.ist.mospolytech.ru');
 let db;
 let filteredDb;
 
+let setArr;
+
+let subTotal;
+
 async function getListCafe() {
     let url = new URL('/api/restaurants', mainUrl);
     url.searchParams.set('api_key', api_key);
@@ -22,6 +26,8 @@ async function getListCafe() {
     }
 
 }
+
+
 function loader(json) {
     db =sortByRating(json);
     filteredDb = db;
@@ -91,8 +97,41 @@ function renderFilterElement(list, type) {
     }
 }
 
-function choiceBtnHandler(event) {
-    console.log('Hello')
+async function getMenuList() {
+    let url = new URL('http://get-menu.std-1678.ist.mospolytech.ru/menu.json');
+    let response = await fetch(url);
+    let json = await response.json();
+
+    if (json.error) {
+        showAlert(json.error);
+    }
+    else {
+      return json;
+    }
+}
+
+function renderMenu(json) {
+
+    setArr = json;
+    let container = document.querySelector('.menu-container');
+
+    for (let unit of json) {
+        let newMenuElement = document.querySelector('.menu-unit').cloneNode(true);
+       
+        newMenuElement.querySelector('.card-text').innerHTML = unit.name;
+
+        newMenuElement.querySelector(`.${unit.type}`).classList.remove('d-none');
+        
+
+        newMenuElement.querySelector('strong').id = unit.id;
+        newMenuElement.classList.remove('d-none');
+        newMenuElement.querySelector('.plus').onclick = plusBtnHandler;
+        newMenuElement.querySelector('.minus').onclick = minusBtnHandler;
+        // for (let btn of newTaskElement.querySelectorAll('.btn-count')) {
+        //     btn.onclick = counterBtnHandler;
+        // }
+        container.append(newMenuElement);
+    }
 }
 
 function pagination() {
@@ -155,7 +194,7 @@ function pageBtnHandler(event) {
 function findBtnHandler(event) {
     filteredDb = []
     let form = event.target.closest('form');
-    console.log('find');
+
     for (let i of db) {
         if ((form.elements['admArea'].value == 'Не задано' || 
             form.elements['admArea'].value == i.admArea) &&
@@ -184,20 +223,77 @@ function getCafeByAddress(address){
 function choiceBtnHandler(event) {
     let address = event.target.closest('tr').querySelector('.cafe-address');
 
-    console.log(address);
 
     let cafe = getCafeByAddress(address.innerHTML);
-    document.querySelector(`#set-1`).innerHTML = cafe[0].set_1;
-    document.querySelector(`#set-2`).innerHTML = cafe[0].set_2;
-    document.querySelector(`#set-3`).innerHTML = cafe[0].set_3;
-    document.querySelector(`#set-4`).innerHTML = cafe[0].set_4;
-    document.querySelector(`#set-5`).innerHTML = cafe[0].set_5;
-    document.querySelector(`#set-6`).innerHTML = cafe[0].set_6;
-    document.querySelector(`#set-7`).innerHTML = cafe[0].set_7;
-    document.querySelector(`#set-8`).innerHTML = cafe[0].set_8;
-    document.querySelector(`#set-9`).innerHTML = cafe[0].set_9;
-    document.querySelector(`#set-10`).innerHTML = cafe[0].set_10;
+    document.querySelector(`#set_1`).innerHTML = cafe[0].set_1;
+    document.querySelector(`#set_2`).innerHTML = cafe[0].set_2;
+    document.querySelector(`#set_3`).innerHTML = cafe[0].set_3;
+    document.querySelector(`#set_4`).innerHTML = cafe[0].set_4;
+    document.querySelector(`#set_5`).innerHTML = cafe[0].set_5;
+    document.querySelector(`#set_6`).innerHTML = cafe[0].set_6;
+    document.querySelector(`#set_7`).innerHTML = cafe[0].set_7;
+    document.querySelector(`#set_8`).innerHTML = cafe[0].set_8;
+    document.querySelector(`#set_9`).innerHTML = cafe[0].set_9;
+    document.querySelector(`#set_10`).innerHTML = cafe[0].set_10;
 
+    document.querySelector('#modal-name').innerHTML = cafe[0].name;
+    document.querySelector('#modal-address').innerHTML = cafe[0].address;
+    document.querySelector('#modal-admArea').innerHTML = cafe[0].admArea;
+    document.querySelector('#modal-district').innerHTML = cafe[0].district;
+    document.querySelector('#modal-rating').innerHTML = cafe[0].rate;
+
+
+}
+
+function getSetById(setId) {
+    return setArr.filter(
+        function(setArr) {
+            return setArr.id == setId;
+        }
+    );
+}
+
+function plusBtnHandler(event) {
+   let counter = event.target.closest('.counters').querySelector('.counter');
+   counter.value = Number(counter.value) + 1 ;
+
+//    console.log(event.target.closest('.card-bottom').querySelector('strong').id)
+   let setId = event.target.closest('.card-bottom').querySelector('strong').id;
+   let set = getSetById(setId);
+   console.log(set, "pizda");
+
+   let flag = true;
+
+   let arr = document.querySelectorAll('modal-set-name');
+   console.log(arr, 'arr')
+   for (let elem of arr) {
+       console.log(set.name, 'hui');
+       if (elem.innerHTML == set.name) flag = false;
+   }
+
+   if (flag) {
+        console.log(flag, 'che');
+        let newModalSet = document.querySelector('.modal-set-template').cloneNode(true);
+        newModalSet.querySelector('.modal-set-name').innerHTML = set.name;
+        console.log(newModalSet);
+        console.log(set.type);
+        console.log(newModalSet.querySelector(`.${set.type}`).classList)
+        newModalSet.querySelector(`.${set.type}`).classList.remove('d-none');
+        newModalSet.querySelector('.set-price').innerHTML = document.querySelector(`.${set.id}`).innerHTML;
+        newModalSet.querySelector('.set-number').innerHTML = counter.value;
+        newModalSet.querySelector('.sub-total').innerHTML = counter.value * document.querySelector(`.${set.id}`).innerHTML;
+   }
+   else {
+
+   }
+  
+}
+
+
+function minusBtnHandler(event) {
+    let counter = event.target.closest('.counters').querySelector('.counter');
+    counter.value = counter.value - 1;
+    if (counter.value < 0) counter.value = 0;
 }
 
 function showAlert(msg, category='alert-danger') {
@@ -211,13 +307,22 @@ function showAlert(msg, category='alert-danger') {
     newAlertElement.classList.remove('d-none');
     alertsContainer.append(newAlertElement);
 }
+
+
+
 window.onload = function () {
     getListCafe()
         .then(loader)
         .catch(showAlert)
     
 
+    getMenuList()
+        .then(renderMenu)
+        .catch(showAlert)
+    
     document.querySelector('.pagination').onclick = pageBtnHandler;
     document.querySelector('#btn-find').onclick = findBtnHandler;
     document.querySelector('#btn-choice').onclick = choiceBtnHandler;
+    // document.querySelector('.plus').onclick = plusBtnHandler;
+    // document.querySelector('.minus').onclick = minusBtnHandler;
 }
